e1c0811af277d84b0c3ef3190a99d7ae225265a1
EDWIN AU <edwinau@rogers.com>
2017-05-01 14:23:40 -0400

    Binding a click event step 1 and 2

--- ajax.js
+++ ajax.js
@@ -1,13 +1,27 @@
 $(document).ready(function () {
 
     /* Your code goes here */
-  $('#run-ajax').on('click', function() {
+  $('#step12').on('click', function() {
     console.log('Before .ajax');
 
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/',
       method: 'GET',
       dataType: 'html',
+    }).done(function(data) {
+      console.log('.ajax done');
+      $('#step12').append(data);
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
     });
+
+    console.log('After .ajax');
   });
+
+
+
+
 });
